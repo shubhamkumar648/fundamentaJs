3f9b52a9cf416b94c8c09a665b313e06080bb041
shubham <ciscoshubham123@gmail.com>
2021-09-15 20:49:38 +0530

    build program using funtion medium level

--- function.js
+++ function.js
@@ -102,4 +102,76 @@ function power (a,b) {
     
     typesOfTriangle(60,60,60)
     
+// -------------------- Medium---------------------------------------------
+
+// Given an array, your function should return the length of the array.
+// Example:
+// Input: arrayLength([1,5,3,7,8]) ––> Output: 5
+         
+   function ArrayOfLength(arr) {
+       
+     return arr.length
+        
+   }
+
+   console.log(ArrayOfLength([1,5,3,7,8]));
+
+
+
+
+// Given an array and an item, your function should return the index at which the item is present.
+// Example:
+// Input: indexOf([1,6,3,5,8,9], 3) ––> Output: 2
+
+    function indexOfArr( arr,item) {
+
+    return arr.indexOf(item)
+    }
+     
+    console.log( indexOfArr([1,6,3,5,8,9], 3));
+
+
+// Given an array and two numbers, your function should replace all entries of first number in an array with the second number.
+// Example:
+// Input: replace([1,5,3,5,6,8], 5, 10) ––> Output: [1,10,3,10,6,8]
+
+
+// function replaceArr(arr,n1,item) {
+
+// //   return   array.splice(array.indexOf(valueToReplace), 1, newValue);
+//   return  arr.splice(index,n1,item);
+// }
+
+console.log(replaceArr([1,5,3,5,6,8],5,10));
+
+// Given two arrays, your function should return single merged array.
+// Example:
+// Input: mergeArray([1,3,5], [2,4,6]) ––> Output: [1,3,5,2,4,6]
+
+function  mergerArray(arr1,arr2) {
+
+    let result = arr1.concat(arr2)
     
+    return result
+}
+    
+console.log(mergerArray([1,3,5] , [2,4,6]))
+// Given a string and an index, your function should return the character present at that index in the string.
+// Example:
+// Input: charAt("neoGcamp", 4) ––> Output: c
+
+function getChar(str,index) {
+
+    return str.charAt(index)
+}
+
+console.log(getChar("neoGcamp" ,4));
+    
+// Given two dates, your function should return which one comes before the other.
+// Example:
+// Input: minDate('02/05/2021', '24/01/2021') ––> Output: 24/01/2021
+
+function minDate() {
+
+
+}
