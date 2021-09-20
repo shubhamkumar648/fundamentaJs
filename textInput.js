1c019352aa0aa6ca4dee83733d8c7f94e23acddd
shubham <ciscoshubham123@gmail.com>
2021-09-20 18:59:44 +0530

    solve heading  question

---
+++ textInput.js
@@ -0,0 +1,30 @@
+const textInput = document.querySelector("#text-Input");
+const submitInputbtn = document.querySelector("#submit-Input");
+const showOutput = document.querySelector("#show-Output")
+const heading1 = document.querySelector("#h1")
+const heading2 = document.querySelector("#h2")
+const heading3 = document.querySelector("#h3")
+
+
+submitInputbtn.addEventListener("click", () => {
+
+  showOutput.innerHTML = textInput.value
+  showOutput.style.fontWeight = "bolder"
+
+
+})
+
+heading1.addEventListener("click",() => 
+
+showOutput.style.fontSize = "32px",
+
+)
+heading2.addEventListener("click",() => 
+
+showOutput.style.fontSize = "24px"
+
+)
+heading3.addEventListener("click",() => 
+
+showOutput.style.fontSize = "18.72px"
+)
