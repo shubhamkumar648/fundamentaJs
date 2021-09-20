e2b0a171b93ce5b3454b6745037b6dc6cd13d52d
shubham <ciscoshubham123@gmail.com>
2021-09-20 19:45:32 +0530

    solve textColor  question

---
+++ textColor.js
@@ -0,0 +1,13 @@
+const showOutput2 = document.querySelector("#Output-text")
+const showInput2= document.querySelector("#text-Input2")
+const submitInputbtn2 = document.querySelector("#submit-Input2")
+
+const red = document.querySelector("#red")
+const green = document.querySelector("#green")
+const blue = document.querySelector("#blue")
+
+submitInputbtn2.addEventListener("click", () => showOutput2.innerHTML = showInput2.value)
+red.addEventListener("click", () => showOutput2.style.color = "red")
+green.addEventListener("click", () => showOutput2.style.color = "green")
+blue.addEventListener("click", () => showOutput2.style.color = "blue")
+
--- textInput.js
+++ textInput.js
@@ -1,30 +1,15 @@
 const textInput = document.querySelector("#text-Input");
 const submitInputbtn = document.querySelector("#submit-Input");
-const showOutput = document.querySelector("#show-Output")
-const heading1 = document.querySelector("#h1")
-const heading2 = document.querySelector("#h2")
-const heading3 = document.querySelector("#h3")
-
+const showOutput = document.querySelector("#show-Output");
+const heading1 = document.querySelector("#h1");
+const heading2 = document.querySelector("#h2");
+const heading3 = document.querySelector("#h3");
 
 submitInputbtn.addEventListener("click", () => {
+  showOutput.innerHTML = textInput.value;
+  showOutput.style.fontWeight = "bolder";
+});
 
-  showOutput.innerHTML = textInput.value
-  showOutput.style.fontWeight = "bolder"
-
-
-})
-
-heading1.addEventListener("click",() => 
-
-showOutput.style.fontSize = "32px",
-
-)
-heading2.addEventListener("click",() => 
-
-showOutput.style.fontSize = "24px"
-
-)
-heading3.addEventListener("click",() => 
-
-showOutput.style.fontSize = "18.72px"
-)
+heading1.addEventListener("click", () => (showOutput.style.fontSize = "32px"));
+heading2.addEventListener("click", () => (showOutput.style.fontSize = "24px"));
+heading3.addEventListener("click",() => (showOutput.style.fontSize = "18.72px"));
