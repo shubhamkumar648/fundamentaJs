a36c762a9ebb080f3b2c022ebc0e9eefc928740d
shubham <ciscoshubham123@gmail.com>
2021-09-29 13:21:36 +0530

    improvise callback method

--- callback.js
+++ callback.js
@@ -24,20 +24,24 @@ calculator(5,2,Mydisplay)
 
 function Information(question,name,designation) {
 
-    console.log(question,name,designation);
+    console.log(question,name(),designation());
 
 }
 
-// function infoName() {
+function infoName() {
 
-//    return ("my name is shubham ");
-// }
+   return ("my name is shubham ");
+}
 
-// function infoDesignation () {
+function infoDesignation () {
 
-//    return   (" i am frontend software devloer ");
-// }
+     return (" i am frontend software devloer ");
+}
+
+// note : - when you pass a function as an argument remember not to use parenthesise () 
+// Information(" tell me  your short intro? ", infoName() ,infoDesignation())  --- wrong  ❌❌
+// Information(" tell me  your short intro? ", infoName ,infoDesignation) --- correct   ✔️✔️
 
 
-Information(" tell me  your short intro? ", () => {"my name is shubham"} ,() => " i am software devloper")
-   
+Information(" tell me  your short intro? ", infoName ,infoDesignation)
+// Information(" tell me  your short intro? ", () => "my name is shubham" ,() => " i am software devloper")
