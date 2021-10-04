9144d9304429eef01177dce13423bce35ffa607b
shubham <ciscoshubham123@gmail.com>
2021-10-04 12:30:54 +0530

    practice api

---
+++ dummyApi.js
@@ -0,0 +1,42 @@
+let dummyBtn = document.querySelector('#dummy-btn')
+
+
+
+// function getData () {
+
+//  let url = "sk.txt"
+
+// fetch(url)
+// .then(response => response.text())
+// .then(data => console.log(data))
+
+// }
+
+function getData () {
+
+let url = "https://dummy.restapiexample.com/api/v1/create";
+let data =  '{"name":"sk42424","salary":"123","age":"23"}'
+
+ let params = {
+
+    method: 'post',
+
+headers:  {
+
+    "content-Type": "application/json"
+},
+
+body: data
+
+}
+
+fetch (url,params) 
+.then(response => response.json())
+.then(data => console.log(data))
+
+}
+
+
+
+
+dummyBtn.addEventListener('click',getData)
