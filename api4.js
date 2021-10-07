0465ad70646153cf65acd1108bc0cf33f31a91fc
shubham <ciscoshubham123@gmail.com>
2021-10-07 15:24:53 +0530

    modify api problem

--- api4.js
+++ api4.js
@@ -1,25 +1,34 @@
-// Here’s an API. Fetch the name and age of user and show them on the web page. Now check weather the user is eligible for vaccination ( must be above 45 ). If yes, show the name in green colour. API 
+// Here’s an API. Fetch the name and age of user and show them on the web page. Now check weather the user is eligible for vaccination ( must be above 45 ). If yes, show the name in green colour. API
 
-const userName = document.querySelector('#user-Name');
+const userName = document.querySelector("#user-Name");
 
-const userAge = document.querySelector('#user-Age');
+const userAge = document.querySelector("#user-Age");
 
-const apicallBtn = document.querySelector("#apiCall-btn")
+const apicallBtn = document.querySelector("#apiCall-btn");
 
-let url = 'https://randomuser.me/api/?results=10'
+let url = "https://randomuser.me/api/?results=10";
 
+let data = [];
 
-let showData = []
+let setData = () => {
+    
+    data.map((user, index)=>{  
+    
+    <div>
 
+      <ul>
+        <li key={index}></li>
+        <li>{user.name.first}</li>
 
+        <li>{user.dob.age}</li>
+      </ul>
+    </div> 
+});
+};
 
-apicallBtn.addEventListener( "click",()=> {
-
-
-
-    fetch(url)
-    .then(response => response.json())
-    .then(json => console.log(showData[json.results.name]))
-    .catch(error => console.error("not fetching  api.." +error))
-
-})
+apicallBtn.addEventListener("click", () => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => console.log(setData((json.results))))
+    .catch((error) => console.error("not fetching  api.." + error));
+});
